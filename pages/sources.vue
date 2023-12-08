01a7bcbbb2651968ac2423c3bbea371bd1c89b70
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-12-08 21:20:25 +0530

    x

--- pages/sources.vue
+++ pages/sources.vue
@@ -6,16 +6,13 @@ let source: any[];
 // const { data: newspurl } = await useFetch(purl)
 // source = newspurl.value.sources
 
-let countries = ['us', 'in']
 
-for (let i = 0; i < countries.length; i++) {
-    let purl = `https://newsapi.org/v2/top-headlines/sources?country=${countries[i]}&apiKey=${runTC.public.secret}`;
-    console.log(purl)
-    const { data: newspurl } = await useFetch(purl)
-    //@ts-ignore
-    source = newspurl.value.sources
-    console.log(source)
-}
+let purl = `https://newsapi.org/v2/top-headlines/sources?&apiKey=${runTC.public.secret}`;
+console.log(purl)
+const { data: newspurl } = await useFetch(purl)
+//@ts-ignore
+source = newspurl.value.sources
+console.log(source)
 </script>
 
 <template>
