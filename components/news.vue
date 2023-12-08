acffa7336bd598222aa0a644b660b0e262aba961
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-12-08 20:07:05 +0530

    search bar done

--- components/news.vue
+++ components/news.vue
@@ -5,20 +5,19 @@ const runTC = useRuntimeConfig()
 
 let q = ref('')
 
-let arr;
-// let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${runTC.public.secret}`;
-// const { data: news } = await useFetch(url)
-// //@ts-ignore
-// arr = news.value.articles
+let arr = ref([]);
+let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${runTC.public.secret}`;
+const { data: news } = await useFetch(url)
+//@ts-ignore
+arr.value = news.value.articles
 
 async function fetchForCategory() {
-
     var url = `https://newsapi.org/v2/everything?q=${q.value}&apiKey=${runTC.public.secret}`;
 
-    const { data: news } = await useFetch(url)
+    const { data: news, refresh } = await useFetch(url)
     console.log(news)
     //@ts-ignore
-    arr = news.value.articles
+    arr.value = news.value.articles
     console.log(arr)
 }
 </script>
@@ -26,28 +25,24 @@ async function fetchForCategory() {
 <template>
     <div class="img mt-5  md:mt-0 relative -top-24 sm:-top-24 md:-top-16 lg:-top-20 lg:mt-2">
         <input type="text" v-model="q" name="q">
-        <button :onclick="fetchForCategory">Search</button>
-        <!-- <UInput v-model="q" name="q" class="md:w-96 lg:w-96 w-full mx-auto" color="blue" v-on:input="fetchForCategory"
-            placeholder="Search By Topic/Channel..." icon="i-heroicons-magnifying-glass-20-solid" autocomplete="off"
-            :ui="{ icon: { trailing: { pointer: '' } } }">
-            <template #trailing>
-                <UButton v-show="q !== ''" color="gray" variant="link" icon="i-heroicons-x-mark-20-solid" :padded="false"
-                    @click="q = ''" />
-            </template>
-        </UInput> -->
-
+        <input type="submit" @click="fetchForCategory()" value="Search">
+        <!-- <button @click="fetchForCategory()">Search</button> -->
     </div>
     <h1 class="p-5 text-3xl underline">Top Headlines</h1>
     <div
         class="sm:grid sm:grid-cols-3 sm:gap-4 sm:p-5 md:grid md:grid-cols-3 md:gap-4 md:p-5 lg:grid lg:grid-cols-3 lg:gap-4 lg:p-5 p-5">
         <div v-for="newss in arr" class="">
+            <!-- @vue-ignore -->
             <div class="bg-gray-800 p-3 rounded-md my-5">
+                <!-- @vue-ignore -->
                 <a :href="newss.url" target="_blank">
                     <div class="img w-full h-56">
+                        <!-- @vue-ignore -->
                         <img v-if="newss.urlToImage != null" :src="newss.urlToImage" alt="" class="h-full w-full">
                         <UAvatar v-else icon="i-heroicons-photo" class="w-full h-full" size="3xl"></UAvatar>
                     </div>
-                    <p class="my-2">{{ newss.title }}</p>
+                    <p class="my-2"> {{ newss.title }}</p>
+                    <p class="my-2">{{ newss.publishedAt }}</p>
                 </a>
             </div>
         </div>
